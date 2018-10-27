99eaf09d46ad834d2440086e89cb40224387f2c9
Alex <sascha.panin2011@yandex.ru>
2018-10-27 23:09:42 +0500

    Revert "фыа"
    This reverts commit 2e5ab5b80607cc31f6805bd9b006e3de788962ff.

--- src/components/AddNewModel.vue
+++ src/components/AddNewModel.vue
@@ -76,6 +76,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <!-- Upload file image -->
                     <v-card class="elevation-0">
                         <h2 class="font-weight-thin mb-4">Select item pic</h2>
@@ -97,6 +98,8 @@
                     <v-btn flat @click="e1 = 1">Cancel</v-btn>
                 </v-stepper-content>
                 <v-stepper-content step="3">
+=======
+>>>>>>> parent of 2e5ab5b... фыа
 =======
 >>>>>>> parent of 2e5ab5b... фыа
 =======
@@ -139,11 +142,14 @@ export default {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             imageLoad: false,
             modelLoad: false,
             required: [
                 v => !!v || 'Required'
             ],
+=======
+>>>>>>> parent of 2e5ab5b... фыа
 =======
 >>>>>>> parent of 2e5ab5b... фыа
 =======
@@ -171,6 +177,7 @@ export default {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         cancel(){
             this.name = ''
@@ -181,6 +188,8 @@ export default {
             this.image = null
             this.tags = []
         },
+=======
+>>>>>>> parent of 2e5ab5b... фыа
 =======
 >>>>>>> parent of 2e5ab5b... фыа
 =======
@@ -206,11 +215,14 @@ export default {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 }
                 if (typeof this.urlImg !== 'object') {
                     if (typeof this.urlImg.i !== 'string') {
                         this.urlImg = snapshot.ref.getDownloadURL()
                         this.imgTrue = true
+=======
+>>>>>>> parent of 2e5ab5b... фыа
 =======
 >>>>>>> parent of 2e5ab5b... фыа
 =======
@@ -252,8 +264,11 @@ export default {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             }
 
+=======
+>>>>>>> parent of 2e5ab5b... фыа
 =======
 >>>>>>> parent of 2e5ab5b... фыа
 =======
@@ -273,12 +288,15 @@ export default {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <style>
     #step{
         margin: 200px 0 50px 0;
     }
 #model_upload, #image_upload {
 =======
+=======
+>>>>>>> parent of 2e5ab5b... фыа
 =======
 >>>>>>> parent of 2e5ab5b... фыа
 =======
